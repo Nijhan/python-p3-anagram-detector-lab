99e4a629ee1093e6fa33a38e48303b0dda11f954
Nijhan <najmashaaban6@gmail.com>
2025-08-13 23:53:46 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        """Return a list of anagrams from candidates."""
+        sorted_word = sorted(self.word)
+        return [c for c in candidates if sorted(c.lower()) == sorted_word and c.lower() != self.word]
